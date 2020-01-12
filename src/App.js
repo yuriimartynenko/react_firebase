88f6e7335ef5c3c2d7d373ab80eb7dccf10bfe67
Yurii Martynenko <ymartinenko4@gmail.com>
2020-01-12 14:37:28 +0200

    bug fixed Alert

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { routes } from './routes/index';
-import AlertContainer from './containers/AlertContainer/index'
+import { routes } from './routes';
+import AlertContainer from './containers/AlertContainer'
 import configureStore from './configureStore';
 
 const store = configureStore();
